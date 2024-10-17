ffe48a548ca94df005c2899fc4acc2f3eb6ff87f
Zack Williams <zackwilldev@gmail.com>
2024-10-17 12:47:13 -0400

    Stopped tracking env.local, and env.staging

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,64 +7,36 @@ set -e  # Exit immediately if a command exits with a non-zero status.
 # Print the current working directory for debugging
 echo "Current working directory: $(pwd)"
 
-RPC_HOST="localhost"
-RPC_PORT=5050
-RPC_URL=http://$RPC_HOST:$RPC_PORT
-
+RPC_URL=https://starknet-sepolia.public.blastapi.io/rpc/v0_7
 echo "${RPC_URL}"
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-WORK_DIR="$(dirname "$SCRIPT_DIR")"
-
-OUTPUT_DIR=$HOME/.kudos
-TIMESTAMP=$(date +%s)
-LOG_DIR=$OUTPUT_DIR/logs/$TIMESTAMP
-TMP_DIR=$OUTPUT_DIR/tmp/$TIMESTAMP
-
-mkdir -p $LOG_DIR
-mkdir -p $TMP_DIR
-
-if [[ $1 == "--clean" ]]; then
-    rm -rf $OUTPUT_DIR/logs/*
-    rm -rf $OUTPUT_DIR/tmp/*
-fi
-
-ACCOUNT_NAME=kudos_acct
-ACCOUNT_ADDRESS=0x328ced46664355fc4b885ae7011af202313056a7e3d44827fb24c9d3206aaa0
-ACCOUNT_PRIVATE_KEY=0x856c96eaa4e7c40c715ccc5dacd8bf6e
-ACCOUNT_PROFILE=starknet-devnet
+ACCOUNT_NAME=kudos_acct2
+ACCOUNT_ADDRESS=0x005f5b3A56446089b4f07f5469a9feCA5Cb7A3e217538eBB1297c34Fd8755c83
+ACCOUNT_PRIVATE_KEY=0x06d0f06604e9cbe45aceaa8682c0551ac683fb2a2dd32c5578392b1badbb4921
+ACCOUNT_PUBLIC_KEY=Kuc28arUWEWAxJXY8Y4WPCv2UzVGRucKarBVz3DKqse
+ACCOUNT_CLASS_HASH=0x036078334509b514626504edc9fb252328d1a240e4e948bef8d0c08dff45927f
+ACCOUNT_TYPE=argent
 ACCOUNT_FILE=$TMP_DIR/starknet_accounts.json
-
-# # Create the accounts file if it doesn't exist
-# if [ ! -f "$ACCOUNT_FILE" ]; then
-#     echo "Creating accounts file..."
-#     sncast --accounts-file $ACCOUNT_FILE account create --name $ACCOUNT_NAME --type argent --add-profile $ACCOUNT_PROFILE --url $RPC_URL --class-hash 0x29927c8af6bccf3f6fda035981e765a7bdbf18a2dc0d630494f8758aa908e2b
-#     echo "accounts file created"
-# fi
-
-# # Add account only if it doesn't already exist
-# if ! sncast --accounts-file $ACCOUNT_FILE account list | grep -q "$ACCOUNT_NAME"; then
-#     sncast --accounts-file $ACCOUNT_FILE account add --name $ACCOUNT_NAME --address $ACCOUNT_ADDRESS --type argent --private-key $ACCOUNT_PRIVATE_KEY --url $RPC_URL --add-profile starknet-devnet
-# else
-#     echo "Account '$ACCOUNT_NAME' already exists. Skipping addition."
-# fi
-echo "sncast --accounts-file $ACCOUNT_FILE account add --url $RPC_URL --name $ACCOUNT_NAME --address $ACCOUNT_ADDRESS --private-key $ACCOUNT_PRIVATE_KEY"
+SCARB_MANIFEST_PATH=/Users/zackwilliams/Development/kudos/contracts/Scarb.toml
 
 
-sncast --accounts-file $ACCOUNT_FILE account add --url $RPC_URL --class-hash 0x61dac032f228abef9c6626f995015233097ae253a7f72d68552db02f2971b8f --name $ACCOUNT_NAME --address $ACCOUNT_ADDRESS --private-key $ACCOUNT_PRIVATE_KEY --type oz
+echo "import account...."
+echo "sncast account import \
+    --url $RPC_URL \
+    --name $ACCOUNT_NAME \
+    --address $ACCOUNT_ADDRESS \
+    --private-key $ACCOUNT_PRIVATE_KEY \
+    --type $ACCOUNT_TYPE \
+    --add-profile $ACCOUNT_NAME"
 
-CONTRACT_DIR="$WORK_DIR/contracts/src"
-KUDOS_CLASS_NAME="Kudos"
-
-# Declare the contract
-
-echo "sncast --accounts-file $ACCOUNT_FILE --account $ACCOUNT_NAME --wait --json declare --contract-name $KUDOS_CLASS_NAME --url $RPC_URL --version v3"
-
-KUDOS_CLASS_DECLARE_RESULT=$(cd $CONTRACT_DIR && sncast --accounts-file $ACCOUNT_FILE --account $ACCOUNT_NAME --wait --json declare --contract-name $KUDOS_CLASS_NAME --url $RPC_URL --version v3| tail -n 1)
-echo "sncast --accounts-file $ACCOUNT_FILE --account $ACCOUNT_NAME --wait --json declare --contract-name $KUDOS_CLASS_NAME --url $RPC_URL --version v3"
-echo $KUDOS_CLASS_DECLARE_RESULT
-KUDOS_CLASS_HASH=$(echo $KUDOS_CLASS_DECLARE_RESULT | jq -r '.class_hash')
-echo "Declared class \"$KUDOS_CLASS_NAME\" with hash $KUDOS_CLASS_HASH"
+# Uncomment the following line to actually run the account import
+# sncast account import \
+#     --url $RPC_URL \
+#     --name $ACCOUNT_NAME \
+#     --address $ACCOUNT_ADDRESS \
+#     --private-key $ACCOUNT_PRIVATE_KEY \
+#     --type $ACCOUNT_TYPE \
+#     --add-profile $ACCOUNT_NAME
 
 # Define constructor parameters
 TOKEN_NAME="0x0 0x324092063603 0x5"
@@ -72,8 +44,28 @@ TOKEN_SYMBOL="0x0 0x4932691 0x3"
 
 # Deploy the contract
 CALLDATA=$(echo -n $TOKEN_NAME $TOKEN_SYMBOL)
-echo "Deploying contract \"$KUDOS_CLASS_NAME\"..."
-KUDOS_CONTRACT_DEPLOY_RESULT=$(sncast --accounts-file $ACCOUNT_FILE --account $ACCOUNT_NAME --wait --json deploy --class-hash $KUDOS_CLASS_HASH --constructor-calldata $CALLDATA --url $RPC_URL --version v3 | tail -n 1)
-echo "sncast --accounts-file $ACCOUNT_FILE --account $ACCOUNT_NAME --wait --json deploy --class-hash 0x00103e59a45d084bb4d37a32d7a5dcb6a5525b1d8f0511b411e3340ed03c8aae --constructor-calldata $CALLDATA --url $RPC_URL --version v3"
-KUDOS_CONTRACT_ADDRESS=$(echo $KUDOS_CONTRACT_DEPLOY_RESULT | jq -r '.contract_address')
-echo "Deployed contract \"$KUDOS_CLASS_NAME\" with address $KUDOS_CONTRACT_ADDRESS"
+
+# Declare the contract
+sncast --account $ACCOUNT_NAME \
+    declare \
+    --url $RPC_URL \
+    --fee-token strk \
+    --contract-name Kudos2 \
+
+
+echo "sncast --account $ACCOUNT_NAME \
+    declare \
+    --url $RPC_URL \
+    --fee-token strk \
+    --contract-name Kudos2 \ "
+
+# Deploy the contract
+echo "sncast deploy --class-hash 0x00103e59a45d084bb4d37a32d7a5dcb6a5525b1d8f0511b411e3340ed03c8aae --constructor-calldata $CALLDATA  --fee-token strk --url $RPC_URL"
+
+sncast deploy \
+  --class-hash 0x00103e59a45d084bb4d37a32d7a5dcb6a5525b1d8f0511b411e3340ed03c8aae \
+  --account $ACCOUNT_NAME \
+  --constructor-calldata $CALLDATA \
+  --fee-token strk \
+  --url $RPC_URL
+/Users/zackwilliams/Development/kudos/contracts/Scarb.toml
--- scripts/deploy_starkli.sh
+++ scripts/deploy_starkli.sh
@@ -1,79 +1,58 @@
 #!/bin/bash
 
-RPC_HOST="localhost"
-RPC_PORT=5050
-RPC_URL=http://$RPC_HOST:$RPC_PORT
+ACCOUNT_NAME=kudos_acct
+ACCOUNT_ADDRESS=0x005f5b3A56446089b4f07f5469a9feCA5Cb7A3e217538eBB1297c34Fd8755c83
+ACCOUNT_PRIVATE_KEY=0x06d0f06604e9cbe45aceaa8682c0551ac683fb2a2dd32c5578392b1badbb4921
+ACCOUNT_PUBLIC_KEY=Kuc28arUWEWAxJXY8Y4WPCv2UzVGRucKarBVz3DKqse
+ACCOUNT_CLASS_HASH=0x036078334509b514626504edc9fb252328d1a240e4e948bef8d0c08dff45927f
+ACCOUNT_PROFILE=argent
+ACCOUNT_FILE=/Users/zackwilliams/.kudos/tmp/starknet_accounts.json
+RPC_URL=https://starknet-sepolia.public.blastapi.io/rpc/v0_7
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 PROJECT_ROOT=$SCRIPT_DIR/..
-
-# Load env variable from `.env` only if they're not already set
-if [ -z "$STARKNET_KEYSTORE" ] || [ -z "$STARKNET_ACCOUNT" ]; then
-  source $PROJECT_ROOT/.env
-fi
-
-# Check if required env variables are set, if not exit
-# if [ -z "$STARKNET_KEYSTORE" ]; then
-#   echo "Error: STARKNET_KEYSTORE is not set."
-#   exit 1
-# elif [ -z "$STARKNET_ACCOUNT" ]; then
-#   echo "Error: STARKNET_ACCOUNT is not set."
-#   exit 1
-# fi
-
-# Transform long options to short ones
-for arg in "$@"; do
-  shift
-  case "$arg" in
-    "--help") set -- "$@" "-h" ;;
-    --*) unrecognized_options+=("$arg") ;;
-    *) set -- "$@" "$arg"
-  esac
-done
-
-# Check if unknown options are passed, if so exit
-if [ ! -z "${unrecognized_options[@]}" ]; then
-  echo "Error: invalid option(s) passed ${unrecognized_options[*]}" 1>&2
-  exit 1
-fi
-
-# Parse command line arguments
-while getopts ":h" opt; do
-  case ${opt} in
-    h )
-      display_help
-      exit 0
-      ;;
-    \? )
-      echo "Invalid Option: -$OPTARG" 1>&2
-      display_help
-      exit 1
-      ;;
-    : )
-      echo "Invalid Option: -$OPTARG requires an argument" 1>&2
-      display_help
-      exit 1
-      ;;
-  esac
-done
-
 ONCHAIN_DIR=$PROJECT_ROOT/contracts
-USERNAME_STORE_SIERRA_FILE=$ONCHAIN_DIR/target/dev/kudos_Kudos.contract_class.json
+KUDOS_SIERRA_FILE=$ONCHAIN_DIR/target/dev/kudos_Kudos.contract_class.json
+
+# Ensure tmp directory exists
+mkdir -p $TMP_DIR
+
+# Set account file path within the project tmp directory
+ACCOUNT_FILE=$TMP_DIR/starknet_accounts.json
+
+starkli account fetch $ACCOUNT_ADDRESS \
+      --rpc $RPC_URL \
+      --network sepolia --force \
+      --output $ACCOUNT_FILE \
+
+echo "starkli account fetch $ACCOUNT_ADDRESS \
+      --rpc $RPC_URL \
+      --network sepolia --force \
+      --output $ACCOUNT_FILE \
+"
+
 
 # Build the contract
 echo "Building the contract..."
 cd $ONCHAIN_DIR && scarb build
 
-
 # Declaring the contract
 echo "Declaring the contract..."
 
-USERNAME_STORE_DECLARE_OUTPUT=$(starkli declare --private-key $ACCOUNT_PRIVATE_KEY --watch $USERNAME_STORE_SIERRA_FILE --rpc $RPC_URL --account /Users/zackwilliams/Development/kudos/test.json 2>&1)
-echo starkli declare --private-key $ACCOUNT_PRIVATE_KEY --watch $USERNAME_STORE_SIERRA_FILE --rpc $RPC_URL --account /Users/zackwilliams/Development/kudos/test.json
-USERNAME_STORE_CONTRACT_CLASSHASH=$(echo $USERNAME_STORE_DECLARE_OUTPUT | tail -n 1 | awk '{print $NF}')
-echo "Contract class hash: $USERNAME_STORE_CONTRACT_CLASSHASH"
+KUDOS_DECLARE_OUTPUT=$(starkli declare --private-key $ACCOUNT_PRIVATE_KEY --watch $KUDOS_SIERRA_FILE --rpc $RPC_URL --account $ACCOUNT_FILE)
+echo "starkli declare --private-key $ACCOUNT_PRIVATE_KEY --watch $KUDOS_SIERRA_FILE --rpc $RPC_URL --account $ACCOUNT_FILE"
+KUDOS_CONTRACT_CLASSHASH=$(echo $KUDOS_DECLARE_OUTPUT)
+echo "Contract class hash: $KUDOS_CONTRACT_CLASSHASH"
 
 # Deploying the contract
 echo "Deploying the contract..."
-echo "starkli deploy --network sepolia --keystore $STARKNET_KEYSTORE --account /Users/zackwilliams/Development/kudos/test.json --watch $USERNAME_STORE_CONTRACT_CLASSHASH"
-starkli deploy --network sepolia --keystore $STARKNET_KEYSTORE --account /Users/zackwilliams/Development/kudos/test.json --watch $USERNAME_STORE_CONTRACT_CLASSHASH
+# Define constructor parameters
+TOKEN_NAME="0x0 0x324092063603 0x5"
+TOKEN_SYMBOL="0x0 0x4932691 0x3"
+
+# Deploy the contract
+CALLDATA=$(echo -n $TOKEN_NAME $TOKEN_SYMBOL)
+echo "starkli deploy --rpc $RPC_URL --network sepolia --private-key $ACCOUNT_PRIVATE_KEY --fee-token STRK --account $ACCOUNT_FILE $KUDOS_CONTRACT_CLASSHASH $CALLDATA"
+
+KUDOS_DEPLOY_OUTPUT=$(starkli deploy --rpc $RPC_URL --network sepolia --private-key $ACCOUNT_PRIVATE_KEY --fee-token STRK --account $ACCOUNT_FILE $KUDOS_CONTRACT_CLASSHASH $CALLDATA)
+echo $KUDOS_DEPLOY_OUTPUT
